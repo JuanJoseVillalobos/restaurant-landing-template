6d2ff590c3a11ad1e8b89e89d91c66386e77b6e4
Juan Villalobos <juan.villalobos0125@jala.university>
2026-03-31 20:27:13 -0300

    fix: MongoDB credentials already setted

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            user: process.env.MONGO_USER,
+            pass: process.env.MONGO_PASSWORD
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
