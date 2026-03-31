8fc60209204a3e0ce9e17f83758173cea1f5a2cb
Juan Villalobos <juan.villalobos0125@jala.university>
2026-03-31 10:13:16 -0300

    feat: Setting routes and HTTP methods

---
+++ backend/routes/menuRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+    getMenuItems,
+    getMenuItemById,
+    createMenuItem,
+    updateMenuItem,
+    deleteMenuItem
+} from '../controllers/menuController.js';
+
+const router = express.Router();
+
+router.route('/')
+    .get(getMenuItems)
+    .post(createMenuItem);
+
+router.route('/:id')
+    .get(getMenuItemById)
+    .put(updateMenuItem)
+    .delete(deleteMenuItem);
+
+export default router;
