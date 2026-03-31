69f7885a520e983fc548ccfcb2d686d60744bc23
Juan Villalobos <juan.villalobos0125@jala.university>
2026-03-31 10:17:11 -0300

    config: Adding products modeling

---
+++ backend/models/MenuItem.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const menuItemSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: { type: String, required: true },
+        category: { type: String, required: true },
+        available: { type: Boolean, required: true, default: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema);
+export default MenuItem;
